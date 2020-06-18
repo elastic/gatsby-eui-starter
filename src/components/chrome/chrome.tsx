a52f483b8b1bd79319b2bf188968471f1643ecad
Dave Snider <dave.snider@gmail.com>
2020-06-18 16:03:44 -0700

    update to eui@26.0.0

--- src/components/chrome/chrome.tsx
+++ src/components/chrome/chrome.tsx
@@ -5,40 +5,17 @@
  * See: https://www.gatsbyjs.org/docs/static-query/
  */
 
-import React from 'react';
-
-// @ts-ignore
-import moon from './assets/moon.svg';
-// @ts-ignore
-import sun from './assets/sun.svg';
-
 import {
-  EuiButton,
-  // @ts-ignore
+  EuiPopover,
+  EuiSpacer,
+  EuiAvatar,
   EuiHeader,
-  // @ts-ignore
-  EuiHeaderSection,
-  // @ts-ignore
-  EuiHeaderSectionItem,
-  // @ts-ignore
-  EuiHeaderSectionItemButton,
-  // @ts-ignore
-  EuiHeaderBreadcrumbs,
+  EuiButton,
   EuiHeaderLogo,
-  EuiIcon,
-  // @ts-ignore
-  EuiNavDrawerGroup,
-  // @ts-ignore
-  EuiNavDrawer,
-  EuiHorizontalRule,
-  // @ts-ignore
-  EuiShowFor,
+  EuiHeaderSectionItemButton,
 } from '@elastic/eui';
 
-import { TopLinks } from '../navigation_links/top_links';
-import { SolutionLinks } from '../navigation_links/solution_links';
-import { ExploreLinks } from '../navigation_links/explore_links';
-import { AdminLinks } from '../navigation_links/admin_links';
+import React, { useState, useEffect } from 'react';
 
 export const ThemeContext = React.createContext('dark');
 
@@ -48,118 +25,113 @@ if (localStorage.getItem('theme') === 'dark') {
   require('../../themes/theme_light.scss');
 }
 
-export default class Chrome extends React.Component<any, any> {
-  navDrawerRef: any;
-  initialTheme = localStorage.getItem('theme') === 'light' ? 'light' : 'dark';
+import HeaderUpdates from './header_updates';
 
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      theme: this.initialTheme,
-      themeIsLoading: false,
+export const Chrome = ({ children }: any) => {
+  const initialTheme =
+    localStorage.getItem('theme') === 'dark' ? 'dark' : 'light';
+  const [themeIsLoading, setThemeIsLoading] = useState(false);
+  const [isAccountOpen, setIsAccountOpen] = useState(false);
+  const [isSpacesOpen, setIsSpacesOpen] = useState(false);
+  const [theme, setTheme] = useState(initialTheme);
+  document.body.classList.add('euiBody--headerIsFixed--double');
+
+  useEffect(() => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    return () => {
+      // Clean up the subscription
+      localStorage.setItem('theme', newTheme);
+      setThemeIsLoading(true);
+      window.location.reload();
     };
-  }
+  }, [theme]);
 
-  handleChangeTheme = () => {
-    this.setState(
-      {
-        theme: this.state.theme === 'dark' ? 'light' : 'dark',
-        themeIsLoading: true,
-      },
-      () => {
-        localStorage.setItem('theme', this.state.theme);
-        window.location.reload();
-      }
-    );
-  };
+  const breadcrumbs = [
+    {
+      text: 'Management',
+      href: '#',
+    },
+    {
+      text: 'Users',
+    },
+  ];
 
-  renderLogo() {
-    return (
-      <EuiHeaderLogo
-        iconType="logoElastic"
-        href="/#/chrome/nav-drawer"
-        aria-label="Goes to home"
+  const headers = (
+    <>
+      <EuiHeader
+        theme="dark"
+        position="fixed"
+        sections={[
+          {
+            items: [
+              <EuiHeaderLogo iconType="logoElastic">Elastic</EuiHeaderLogo>,
+            ],
+            borders: 'none',
+          },
+          {
+            items: [
+              <EuiPopover
+                button={
+                  <EuiHeaderSectionItemButton
+                    aria-label="Account menu"
+                    onClick={() => setIsAccountOpen(!isAccountOpen)}>
+                    <EuiAvatar name="John Username" size="s" />
+                  </EuiHeaderSectionItemButton>
+                }
+                isOpen={isAccountOpen}
+                closePopover={() => setIsAccountOpen(false)}>
+                <div>
+                  <EuiButton
+                    size="s"
+                    iconType="invert"
+                    onClick={() =>
+                      setTheme(theme === 'light' ? 'dark' : 'light')
+                    }
+                    isLoading={themeIsLoading}>
+                    Switch Theme
+                  </EuiButton>
+                </div>
+              </EuiPopover>,
+            ],
+            borders: 'none',
+          },
+        ]}
       />
-    );
-  }
+      <EuiHeader
+        position="fixed"
+        sections={[
+          {
+            items: [
+              <EuiPopover
+                button={
+                  <EuiHeaderSectionItemButton
+                    aria-label="Account menu"
+                    onClick={() => setIsSpacesOpen(!isAccountOpen)}>
+                    <EuiAvatar type="space" name="Default Space" size="s" />
+                  </EuiHeaderSectionItemButton>
+                }
+                isOpen={isSpacesOpen}
+                closePopover={() => setIsSpacesOpen(false)}>
+                <div>asdf</div>
+              </EuiPopover>,
+            ],
+            breadcrumbs: breadcrumbs,
+            borders: 'right',
+          },
+          {
+            items: [<HeaderUpdates />],
+            borders: 'none',
+          },
+        ]}
+      />
+    </>
+  );
 
-  renderMenuTrigger() {
-    return (
-      <EuiHeaderSectionItemButton
-        aria-label="Open nav"
-        onClick={() => this.navDrawerRef.toggleOpen()}>
-        <EuiIcon type="apps" href="#" size="m" />
-      </EuiHeaderSectionItemButton>
-    );
-  }
-
-  renderBreadcrumbs() {
-    const breadcrumbs = [
-      {
-        text: 'Home',
-        href: '#',
-        onClick: (e: { preventDefault: () => void }) => {
-          e.preventDefault();
-          console.log('You clicked home');
-        },
-        'data-test-subj': 'breadcrumbsAnimals',
-        className: 'customClass',
-      },
-    ];
-
-    return <EuiHeaderBreadcrumbs breadcrumbs={breadcrumbs} />;
-  }
-
-  setNavDrawerRef = (ref: any) => (this.navDrawerRef = ref);
-
-  render() {
-    const themeIcon = this.state.isDarkTheme ? sun : moon;
-
-    console.log(ExploreLinks);
-    return (
-      <ThemeContext.Provider value={this.state.theme}>
-        <div>
-          <EuiHeader className="chrHeader">
-            <EuiHeaderSection grow={false}>
-              <EuiShowFor sizes={['xs', 's']}>
-                <EuiHeaderSectionItem border="right">
-                  {this.renderMenuTrigger()}
-                </EuiHeaderSectionItem>
-              </EuiShowFor>
-              <EuiHeaderSectionItem border="right">
-                {this.renderLogo()}
-              </EuiHeaderSectionItem>
-              <EuiHeaderSectionItem border="right">
-                {/* <HeaderSpacesMenu /> */}
-              </EuiHeaderSectionItem>
-            </EuiHeaderSection>
-
-            {this.renderBreadcrumbs()}
-
-            <EuiHeaderSection side="right">
-              <EuiHeaderSectionItem className="chrHeader__themeSection">
-                <EuiButton
-                  size="s"
-                  iconType={themeIcon}
-                  onClick={() => this.handleChangeTheme()}
-                  isLoading={this.state.themeIsLoading}>
-                  Switch Theme
-                </EuiButton>
-              </EuiHeaderSectionItem>
-            </EuiHeaderSection>
-          </EuiHeader>
-          <EuiNavDrawer ref={this.setNavDrawerRef}>
-            <EuiNavDrawerGroup listItems={TopLinks} />
-            <EuiHorizontalRule margin="none" />
-            <EuiNavDrawerGroup listItems={ExploreLinks} />
-            <EuiHorizontalRule margin="none" />
-            <EuiNavDrawerGroup listItems={SolutionLinks} />
-            <EuiHorizontalRule margin="none" />
-            <EuiNavDrawerGroup listItems={AdminLinks} />
-          </EuiNavDrawer>
-          <div className="chrWrap">{this.props.children}</div>
-        </div>
-      </ThemeContext.Provider>
-    );
-  }
-}
+  return (
+    <ThemeContext.Provider value={theme}>
+      <EuiSpacer />
+      {headers}
+      {children}
+    </ThemeContext.Provider>
+  );
+};
